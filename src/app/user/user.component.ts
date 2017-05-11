d23e44b0b488d19104b280ccacc5421773362627
Miguel S. Mendoza <miguel@smendoza.net>
2017-05-11 22:40:57 +0200

    Finished

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,29 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { UsersService } from '../users.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   id: number;
+  userSubscription: Subscription;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private usersService: UsersService) { }
 
   ngOnInit() {
-    this.route.params
+    this.userSubscription = this.route.params
       .subscribe(
         (params: Params) => {
           this.id = +params['id'];
         },
-        () => {
-          console.log('ERROR');
-        },
-        () => {
-          console.log('COMPLETE');
-        }
+        // () => {
+        //   console.log('ERROR');
+        // },
+        // () => {
+        //   console.log('COMPLETE');
+        // }
       );
+  }
+
+  public onActivate(): void {
+    this.usersService.userActivated.next(this.id);
+    console.log('penecoonespolla'.match(/pene(.*)polla/));
+  }
+
+  ngOnDestroy() {
+    this.userSubscription.unsubscribe();
   }
 
 }
